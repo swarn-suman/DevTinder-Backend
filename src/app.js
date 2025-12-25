1fb87c687ac1074d7a6fd5aea5aa382dd6f3e700
swarn <swarnsuman004@gmail.com>
2025-12-25 15:21:16 +0530

    created models

--- src/app.js
+++ src/app.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const app =express();
 
-const dbConnect = require('./config/database')
+const dbConnect = require('./config/database');
+const User = require('./models/user');
+
+app.use(express.json())
+
+app.post('/signup', async(req,res)=>{
+    const user = new User(req.body)
+    try{
+        await user.save()
+        res.send("User added successfully")
+    }
+    catch(err){
+        res.status(400).send("Error while saving user")
+    }
+})
 
 dbConnect()
-   .then(()=>{
+  .then(()=>{
     console.log("DB connected successfully")
 
     app.listen(7777,()=>{
-    console.log('App is running at port 7777')
-})
-   })
-
-   .catch((err)=>{
-    console.log("Db not connected")
-   })
+        console.log("server is running at PORT 7777")
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
 
 
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose')
 
 const dbConnect = async()=>{
-    await mongoose.connect('mongodb+srv://swarnsuman_test:swarnBulkMail@cluster0.l4mvirb.mongodb.net/DevTinder')
+    await mongoose.connect("mongodb+srv://swarnsuman_test:swarnBulkMail@cluster0.l4mvirb.mongodb.net/DevTinder")
 }
 
-module.exports = dbConnect;
-
-
-
-
-
-
-
+module.exports = dbConnect
