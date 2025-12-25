bca595ae4b80f450b151236d9f4e4c3b7cc2278d
swarn <swarnsuman004@gmail.com>
2025-12-25 22:34:29 +0530

    added APIs

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const User = require('./models/user');
 
 app.use(express.json())
 
+//signup a new user
 app.post('/signup', async(req,res)=>{
     const user = new User(req.body)
     try{
@@ -16,6 +17,66 @@ app.post('/signup', async(req,res)=>{
         res.status(400).send("Error while saving user")
     }
 })
+
+//get details of a single user
+app.get('/user',async(req,res)=>{
+  const userEmail = req.body.emailId
+
+  try{
+    const user = await User.findOne({emailId:userEmail})
+
+    if(user.length === 0){
+      res.status(401).send("User not found")
+    }
+    else{
+      res.send(user)
+    }
+  }
+
+  catch(err){
+    res.status(400).send("Error while fetching user")
+  }
+})
+
+//get details of all the users
+app.get('/users', async(req,res)=>{
+
+  try{
+    const users = await User.find()
+    res.send(users)
+  }
+  catch(err){
+    res.status(400).send("Error while fetching users")
+  }
+})
+
+//delete a user
+app.delete('/user', async(req,res)=>{
+  const userId = req.body.userId
+
+  try{
+    await User.findByIdAndDelete(userId)
+    res.send("User deleted successfully")
+  }
+  catch(err){
+    res.status(400).send("Error while deleting user")
+  }
+})
+
+//update a user
+app.patch('/user', async(req,res)=>{
+  const firstname = req.body.firstName
+  const data = req.body
+
+  try{
+    await User.findOneAndUpdate({ firstName: firstname },data)
+     res.send("User updated successfully")
+  }
+  catch(err){
+    res.status(400).send("Error while updating user")
+  }
+})
+
 
 dbConnect()
   .then(()=>{
