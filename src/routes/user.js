d8f9843b2d05f3ad34a0cff789081a9e9c887a59
swarn <swarnsuman004@gmail.com>
2025-12-31 00:12:21 +0530

    updated routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -23,4 +23,32 @@ userRouter.get('/user/request/received', userAuth, async(req,res) =>{
     }
 })
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", "firstName lastName photoUrl age gender about skills")
+      .populate("toUserId", "firstName lastName photoUrl age gender about skills");
+
+    console.log(connectionRequests);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+})
+
 module.exports = userRouter
